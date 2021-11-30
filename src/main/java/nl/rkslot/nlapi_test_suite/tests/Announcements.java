77c84bd50877c93dd824fc53896f75abed5408de
Tadhg <tadhgsmboyle@gmail.com>
2021-11-29 22:01:41 -0800

    Add kinda stupid test to announcements and websiteinfo

--- src/main/java/nl/rkslot/nlapi_test_suite/tests/Announcements.java
+++ src/main/java/nl/rkslot/nlapi_test_suite/tests/Announcements.java
@@ -1,17 +1,17 @@
 package nl.rkslot.nlapi_test_suite.tests;
 
 import com.namelessmc.java_api.NamelessAPI;
-import com.namelessmc.java_api.NamelessUser;
+import nl.rkslot.nlapi_test_suite.Test;
 import nl.rkslot.nlapi_test_suite.TestStage;
 import org.jetbrains.annotations.NotNull;
 
 public class Announcements extends TestStage {
 
-    // TODO: Add tests for announcements
-    public void runTest(final @NotNull NamelessAPI api) throws Exception {
-        api.getAnnouncements();
-        NamelessUser admin = api.getUser(1).orElseThrow();
-        api.getAnnouncements(admin);
+    @Test
+    public void test_returns_correct_announcements(final @NotNull NamelessAPI api) throws Exception {
+        assertThat(api.getAnnouncements().size() == 0);
+
+        // TODO: add tests/api endpoint for creating announcements
     }
 
 }
--- src/main/java/nl/rkslot/nlapi_test_suite/tests/WebsiteInfo.java
+++ src/main/java/nl/rkslot/nlapi_test_suite/tests/WebsiteInfo.java
@@ -7,14 +7,21 @@ import org.jetbrains.annotations.NotNull;
 
 public class WebsiteInfo extends TestStage {
 
+    private static final @NotNull String NAMELESS_VERSION = "2.0.0-pr12";
+
     @Test
     public void test_language_matches_English_UK(final @NotNull NamelessAPI api) throws Exception {
-        assertThat(api.getWebsite().getLanguage().equals("EnglishUK"));
+        assertThat(api.getWebsite().getLanguage().equals("EnglishUK"), "expected EnglishUK, got " + api.getWebsite().getLanguage());
     }
 
     @Test
     public void test_module_count_is_correct(final @NotNull NamelessAPI api) throws Exception {
-        assertThat(api.getWebsite().getModules().length > 0);
+        assertThat(api.getWebsite().getModules().length == 4, "expected 4 modules installed by default, got " + api.getWebsite().getModules().length);
+    }
+
+    @Test
+    public void test_website_version_is_correct(final @NotNull NamelessAPI api) throws Exception {
+        assertThat(api.getWebsite().getVersion().equals(NAMELESS_VERSION), "expected " + NAMELESS_VERSION + ", got " + api.getWebsite().getVersion());
     }
 
 }
